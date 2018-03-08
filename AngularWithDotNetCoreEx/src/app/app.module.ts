03aad6d3f8f9682e11f6eccec0895fe6d908a5e4
Edward Apostol <ed@edapostol.com>
2018-03-07 20:48:23 -0500

    #006: added formsmodule and httpmodule
    formsmodule for getting input, httpmodule for http services

--- AngularWithDotNetCoreEx/src/app/app.module.ts
+++ AngularWithDotNetCoreEx/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+
+// add FormsModule for input and HttpModule for http services
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
